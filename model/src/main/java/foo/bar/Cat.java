500669fd363355759889278a70011298d8ddf36e
Ivan Lapunka <ivan_lapunka@gmail.com>
2021-03-22 18:48:03 +0300

    initial commit add gitignore add cat object

---
+++ model/src/main/java/foo/bar/Cat.java
@@ -0,0 +1,10 @@
+package foo.bar;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cat {
+    String name;
+}
---
+++ src/main/java/app/main/Main.java
@@ -0,0 +1,11 @@
+package app.main;
+
+import foo.bar.Cat;
+
+import javax.xml.catalog.Catalog;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat c = new Cat();
+    }
+}
