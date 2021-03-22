cf6532ce5f9ef9e0898d24564fa94c79bcd336f7
Ivan Lapunka <ivan_lapunka@gmail.com>
2021-03-22 23:44:47 +0300

    Added run of init method after object creation @Postconstruct annotation neede to add javax.annotation-api dependency

--- model/src/main/java/foo/bar/Parrot.java
+++ model/src/main/java/foo/bar/Parrot.java
@@ -3,8 +3,15 @@ package foo.bar;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Data
 @Component
 public class Parrot {
     private String name;
+
+    @PostConstruct
+    private void init() {//method run after the object has been created
+        setName("Kesha");
+    }
 }
